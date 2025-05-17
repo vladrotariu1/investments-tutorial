163d62a918e19f2d49fddee7f43543a95e307ff6
vladrotariu1 <inggame95@gmail.com>
2025-05-17 16:42:40 +0300

    moving to public folder

--- src/components/GoToQuestionarePage.tsx
+++ src/components/GoToQuestionarePage.tsx
@@ -6,10 +6,10 @@ function GoToQuestionarePage() {
     }
 
     return (
-        <Hero imageSrc={ 'src/assets/raspunde-la-intrebari.jpg' }>
+        <Hero imageSrc={ 'public/assets/raspunde-la-intrebari.jpg' }>
             <div className="justify-center items-center flex flex-col h-full">
                 <h1 className="text-4xl font-bold text-[#cf0419] font-family-unicredit-bold"> Ce e de fǎcut? </h1>
-                <img className="w-20 m-10" src="src/assets/arrow-down.png" alt="Arrow down"/>
+                <img className="w-20 m-10" src="../../public/assets/arrow-down.png" alt="Arrow down"/>
                 <div
                     onClick={handleOnButtonClick}
                     className="mb-10 max-w-60 cursor-pointer font-bold text-2xl bg-[#e2061d] text-[#fafafa] text-center w-9/12 p-3 rounded-2xl">
--- src/components/UnicreditHeader.tsx
+++ src/components/UnicreditHeader.tsx
@@ -1,7 +1,7 @@
 function UnicreditHeader() {
     return (
         <header className="h-20 border-b-gray-400 border-b-1 flex items-center absolute w-full z-30 bg-white ">
-            <div className="w-60 h-11/12 bg-[url(src/assets/unicredit-logo.png)] bg-center bg-cover"></div>
+            <div className="w-60 h-11/12 bg-[url(public/assets/unicredit-logo.png)] bg-center bg-cover"></div>
             <div className="w-6 h-1 bg-gray-950"></div>
             <p className="text-2xl font-bold ml-4">Pagina ta</p>
         </header>
